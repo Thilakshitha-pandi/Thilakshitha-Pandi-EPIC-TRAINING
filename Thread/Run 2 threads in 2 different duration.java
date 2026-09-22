84a4ca124ab8e1d1ab38c7a1afdd40866eb80d0f
THILAKSHITHA PANDI P <thilakshitha0903@gmail.com>
2026-09-22 16:17:00 +0530

    Implement two threads with different durations
    This Java program creates two threads that run concurrently, each with different sleep durations. Thread 1 sleeps for 1 second and Thread 2 sleeps for 2 seconds while printing their respective counts.

---
+++ Thread/Run 2 threads in 2 different duration.java
@@ -0,0 +1,34 @@
+class MyThread1 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 1: " + i);
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+class MyThread2 extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 2: " + i);
+            try {
+                Thread.sleep(2000); 
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread1 t1 = new MyThread1();
+        MyThread2 t2 = new MyThread2();
+        t1.start();
+        t2.start();
+    }
+}
